72c5cfb74437d5675199620402ae312c39a1d609
ZJULZL <104138024+ZJULZL@users.noreply.github.com>
2022-05-18 18:11:28 +0800

    大改运动判断

--- src/judge.h
+++ src/judge.h
@@ -1,15 +1,23 @@
 #ifndef JUDGE_H
 #define JUDGE_H
+#define JUMPBLOCKRANGEX 0.4
+#define JUMPBLOCKRANGEY 0.1
+#define JUMPDOTRANGEX 0.4
+#define JUMPDOTRANGEY 0.05
+#define MOVERANGE 0.15
 bool distance(double x1, double y1, double size1, double x2, double y2, double size2);//根据坐标和判定半径确定距离
-bool RoleAndGroundX(struct BLOCK* blockhead);//角色与地面分水平和竖直判定
-bool RoleAndGroundY(struct BLOCK* blockhead);
+bool JumpJudgeBlock();
+bool RightMoveJudgeBlock();
+bool LeftMoveJudgeBlock();
 bool RoleAndEnemy(struct ENEMY enemy);//角色和敌人的碰撞判定
 bool RoleAndGoal(struct GOAL goal);
 bool EnemyAndBullet(struct ENEMY enemy, struct BULLET bullet);//敌人和子弹的碰撞判定
-bool BulletAndGround(struct BULLET bullet, struct BLOCK* blockhead);
+bool BulletAndGround(struct BULLET bullet);
 bool RoleAndBonus(struct BONUS bonus);//角色与收集物的判定
-bool RoleAndLineX();//角色与所画桥的判定
-bool RoleAndLineY();
-bool MouseAndGround(struct BLOCK* blockhead);//鼠标位置和地面表面的判定，画图时用
-bool MouseAndLine(struct LINE* line);//右键回收桥梁使用
+bool JumpJudgeDot();
+bool RightMoveJudgeDot();
+bool LeftMoveJudgeDot();
+bool MouseAndGround();//鼠标位置和地面表面的判定，画图时用
+bool MouseAndLine(LINE*line);//右键回收桥梁使用
+bool MouseAndAllLine();
 #endif
